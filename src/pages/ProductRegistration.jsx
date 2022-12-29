ea03524d9d4675911f04666248fcab773f6fbc3f
numb0y <lucaspatriota@outlook.com>
2022-12-29 15:24:07 -0300

    alterações nos botões

--- src/pages/ProductRegistration.jsx
+++ src/pages/ProductRegistration.jsx
@@ -41,7 +41,7 @@ function ProductRegistration() {
     },
   });
 
-  const Item = styled(Paper)(({ theme }) => ({ 
+  const Item = styled(Paper)(({ theme }) => ({
     textAlign: 'center',
     margin: '20px',
   }));
@@ -57,89 +57,97 @@ function ProductRegistration() {
       productPrice,
       imagepath,
       productName
-     
+
     })
   }
 
   return (
-      
+
     <ThemeProvider theme={theme}>
-      <Grid 
+      <Grid
         container spacing={4}
         direction="column"
         justifyContent="center"
         alignItems="center"
-        
+
       >
-        
-        <Grid item xs={12} sx={{ backgroundColor: '#FFA301', padding: 6, width: 428, height: 90
-        , display:'flex', gap: 1, flexDirection:'row'}} >
+
+        <Grid item xs={12} sx={{
+          backgroundColor: '#FFA301', padding: 6, width: 428, height: 90
+          , display: 'flex', gap: 1, flexDirection: 'row'
+        }} >
           <div className='toptitle'>
-          <ArrowCircleLeft size={40} color="white" weight="bold" />
-          <Title title="Cadastrar Produto"/>
+            <ArrowCircleLeft size={40} color="white" weight="bold" />
+            <Title title="Cadastrar Produto" />
           </div>
-          </Grid> 
+        </Grid>
 
 
-        <Grid item xs={12} sx={{height: 90, marginTop: 3, width: 390}} >
-        <SubTitle  sx={{fontFamily: 'Josefin Sans'}}
-          text="Para cadastrar seus produtos," />
-        <Description
-          description="Digite os dados do seu produto para que você aumente suas vendas." />
-          </Grid>
+        <Grid item xs={12} sx={{ height: 90, marginTop: 3, width: 390 }} >
+          <SubTitle sx={{ fontFamily: 'Josefin Sans' }}
+            text="Para cadastrar seus produtos," />
+          <Description
+            description="Digite os dados do seu produto para que você aumente suas vendas." />
+        </Grid>
 
-          <Box>
-          <Grid 
+        <Box>
+          <Grid
             container spacing={3}
             direction="column"
             justifyContent="center"
             alignItems="center"
             padding={4}
-            
+
           >
-          
-      <form action="" onSubmit={submitHandler}>
-        <Grid  rowSpacing={6} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{width:350}}  >
-            <Grid  >
-              <Item>
-                <TextField id="productName" label="Nome" variant="outlined" size='normal' fullWidth />
-              </Item>
-            </Grid>
 
-            <Grid  >
-              <Item>
-                <TextField id="productPrice" label="Preço "   variant="outlined" fullWidth/>
-              </Item>
-            </Grid>
+            <form action="" onSubmit={submitHandler}>
+              <Grid rowSpacing={6} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ width: 350 }}  >
+                <Grid  >
+                  <Item>
+                    <TextField id="productName" label="Nome" variant="outlined" size='normal' fullWidth />
+                  </Item>
+                </Grid>
 
-            <Grid  >
-              <Item>
-                <TextField id="productDescription" label="Descrição" variant="outlined" fullWidth multiline rows={4}/>
-              </Item>
-            </Grid>        
+                <Grid  >
+                  <Item>
+                    <TextField id="productPrice" label="Preço " variant="outlined" fullWidth />
+                  </Item>
+                </Grid>
+
+                <Grid  >
+                  <Item>
+                    <TextField id="productDescription" label="Descrição" variant="outlined" fullWidth multiline rows={4} />
+                  </Item>
+                </Grid>
+
+              </Grid>
+
+
+            </form>
+
           </Grid>
-        <Grid item xs={12}>
-          
-          <Button color='secondary' variant='contained' size='small' sx={{color: 'black'}}>
-            <Camera size={20} color="black" weight="regular" />
-            Upload da imagem
-          </Button>
+
+
+          <Grid item xs={18} sx={{ textAlign: 'center' }} >
+                <Button color='secondary' variant='contained' size='small' sx={{ color: 'black'}}>
+                  <Camera size={20} color="black" weight="regular" />
+                  Upload da imagem
+                </Button>
+          </Grid>
+
+          <Grid item xs={18} sx={{padding: 4, textAlign: 'center' }} >
+                <Button type='submit' color="primary" variant="contained" size='large' sx={{ color: 'white', fontSize: 14 }}>
+                  CONFIRMAR
+                </Button>
+          </Grid>
+          </Box>
         </Grid>
 
-        <Grid item xs={12}>
-        
-          <Button type='submit' color="primary"  variant="contained" size='large' sx={{color: 'white', fontSize: 14}}>
-            CONFIRMAR
-          </Button>
-        </Grid>
-      </form>
 
-        </Grid>
-        </Box>
 
-        </Grid>
-        </ThemeProvider>
-        
+
+    </ThemeProvider>
+
   )
 }
 
